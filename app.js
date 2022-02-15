427c48c25ef0883811eaf54c407f44229e485fb9
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-15 21:20:12 +0100

    Esercizio di oggi completato! Domani 2a parte!

--- app.js
+++ app.js
@@ -1,43 +1,70 @@
+// VARIABILE COLLEGATA AL MAIN
+const mainContainer = document.getElementById('main');
+console.log(main);
+
+// COLLEGO IL GRID-CONTAINER
+const gridContainer = document.querySelector('.grid_container');
+console.log(gridContainer);
+
 // COLLEGO PULSANTE PLAY
 const playButton = document.getElementById('play_button');
 console.log(playButton);
 
 // CREO L'ASCOLTO AL CLICK DEL PULSANTE PLAY
 playButton.addEventListener('click', function () {
-
-    // COLLEGO IL GRID-CONTAINER
-    const gridContainer = document.querySelector('.grid_container');
-    console.log(gridContainer);
+    
+    // REGOLA PER REFRESH DEI LIVELLI DI DIFFICOLTÀ
+    gridContainer.innerHTML = '';
 
     // CREO VARIABILE PER LA DIFFICOLTÀ
     const difficulty = document.getElementById('select_difficulty').value;
     console.log(difficulty);
 
     // CREO LE CELLE DENTRO IL "GRID-CONTAINER" IN BASE ALE VARIE DIFFICOLTÀ
-    if (difficulty == 'Easy') {
+    if (difficulty == 'Scegli') {
+
+        alert('Scegli un livello di difficoltà!');
+
+        console.log('Scegli un livello di difficoltà!');
+
+    } else if (difficulty == 'Easy') {
+
+        console.log(difficulty);
+
         for (let i = 0; i < 100; i++) {
             const cellElement = document.createElement('div');
             cellElement.classList.add('cell');
-            cellElement.style.width=`calc(100% / 10)`;
+            cellElement.style.width = `calc(100% / 10)`;
             gridContainer.append(cellElement);
-            console.log(cellElement);
         }
+
+        mainContainer.style.background = `#87cefa`;
+
     } else if (difficulty == 'Hard') {
+
+        console.log(difficulty);
+
         for (let i = 0; i < 81; i++) {
             const cellElement = document.createElement('div');
             cellElement.classList.add('cell');
-            cellElement.style.width=`calc(100% / 9)`;
+            cellElement.style.width = `calc(100% / 9)`;
             gridContainer.append(cellElement);
-            console.log(cellElement);
         }
-    } else {
+
+        mainContainer.style.background = `#ffd700`;
+
+    } else if (difficulty == 'Crazy') {
+
+        console.log(difficulty);
+
         for (let i = 0; i < 49; i++) {
             const cellElement = document.createElement('div');
             cellElement.classList.add('cell');
-            console.log(cellElement);
+            cellElement.style.width = `calc(100% / 7)`;
             gridContainer.append(cellElement);
-            cellElement.style.width=`calc(100% / 7)`;
         }
+
+        mainContainer.style.background = `#ff4500`;
+
     }
-    
 })
